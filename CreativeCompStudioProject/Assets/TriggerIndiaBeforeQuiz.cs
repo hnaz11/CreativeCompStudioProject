9bf25211829f1f72581fba1ea9c36b608374c25e
saakshinaveen <saakshinaveen@vt.edu>
2022-04-07 13:15:49 -0400

    transition scenes

---
+++ CreativeCompStudioProject/Assets/TriggerIndiaBeforeQuiz.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerIndiaBeforeQuiz : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(7);
+        Debug.Log("triggered....");
+    }
+}
