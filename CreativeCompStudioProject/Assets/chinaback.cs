f1df722aafb59ed81fb071f35a5309fe944899c6
hnaz11 <74683004+hnaz11@users.noreply.github.com>
2022-04-07 13:44:17 -0400

    buttons

---
+++ CreativeCompStudioProject/Assets/chinaback.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class chinaback : MonoBehaviour
+{
+   public void OnButtonPress(){
+        SceneManager.LoadScene(8);
+    }
+}
