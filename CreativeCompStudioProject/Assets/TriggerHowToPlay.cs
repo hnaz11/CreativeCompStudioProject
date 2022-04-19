b5414b8329d6d06e9057a0949c90e16a1aed4a71
nikitapatel3344 <49453379+nikitapatel3344@users.noreply.github.com>
2022-04-18 21:19:29 -0400

    Added country labels in before quiz scenes, routed welcome and how to play buttons

---
+++ CreativeCompStudioProject/Assets/TriggerHowToPlay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHowToPlay : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene(22);
+    }
+}
