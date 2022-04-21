bba7af12ccd4e3c2f19cadf0ed4bcb470b00e8e5
saakshinaveen <saakshinaveen@vt.edu>
2022-04-21 15:11:43 -0400

    airplane fixed for world map

---
+++ CreativeCompStudioProject/Assets/airplaneMoveAfrica.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class airplaneMoveAfrica : MonoBehaviour
+{
+     public float speed = 1.5f;
+     private Vector3 target;
+     private Vector2 mouse;
+ 
+     void Start () {
+         target = transform.position;
+     }
+     
+     void Update () {
+         mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+         if (Input.GetMouseButtonDown(0) && mouse.x > Screen.width / 2 && mouse.y > Screen.height / 2) {
+             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+             target.z = transform.position.z;
+         }
+         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+     } 
+}
