f304d510345c17097761deb66d5b901f3ad285fc
Jakob Schaming <schaming.jakob@gmail.com>
2022-04-25 19:52:59 -0400

    guitar to piano works but not vice versa :|

---
+++ CreativeCompStudioProject/Assets/instrumentTransitionButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class instrumentTransitionButton : MonoBehaviour
+{
+    [SerializeField]
+    private Camera pianoCam;
+    [SerializeField]
+    private Camera guitarCam;
+
+    public void Start() {
+        //Camera.main.enabled = false;
+    }
+
+    public void PianoPress() {
+        pianoCam.enabled = true;
+        //Camera.main.enabled = false;
+        Debug.Log("Piano Pressed!");
+    }
+
+    public void GuitarPress() {
+        guitarCam.enabled = true;
+        Camera.main.enabled = false;
+    }
+}
