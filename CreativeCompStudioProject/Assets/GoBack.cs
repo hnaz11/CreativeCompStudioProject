dbff2651fa9e0d4e347f070f181b3ddaf30dde24
nikitapatel3344 <49453379+nikitapatel3344@users.noreply.github.com>
2022-04-25 21:12:42 -0400

    Changed positions and added buttons

---
+++ CreativeCompStudioProject/Assets/GoBack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GoBack : MonoBehaviour
+{
+    public void goBackGP()
+    {
+        SceneManager.LoadScene(16);
+    }
+
+    public void goBackNO()
+    {
+        SceneManager.LoadScene(18);
+    }
+
+    public void goBackPeru()
+    {
+        SceneManager.LoadScene(26);
+    }
+
+    public void goBackCuba()
+    {
+        SceneManager.LoadScene(25);
+    }
+
+    public void goBackZimbabwe()
+    {
+        SceneManager.LoadScene(14);
+    }
+    public void goBackAlgeria()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void goBackIndia()
+    {
+        SceneManager.LoadScene(10);
+    }
+}
